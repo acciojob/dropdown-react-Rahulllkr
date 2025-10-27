bc3640fc5e7f3b3262b892a4afdc55393171939b
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-28 00:10:24 +0530

    try now

--- src/components/App.js
+++ src/components/App.js
@@ -193,11 +193,11 @@ function App(){
 		<div id="state-description">{selectedState.description}</div>
 		
 
-		<div id="city-name">{selectedCity.name}</div>
+		<div id="city-title">{selectedCity.name}</div>
 		<div id="city-description">{selectedCity.description}</div>
 		
 
-		<div id="landmark-name">{selectedLandmark.name}</div>
+		<div id="landmark-title">{selectedLandmark.name}</div>
 		<div id="landmark-description">{selectedLandmark.description}</div>
 		
 	</div>
